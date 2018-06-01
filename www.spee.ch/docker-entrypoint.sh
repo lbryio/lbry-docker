221554c6e9a02f8a200a827a92b61838cf06e277
Leopere <colin@nixc.us>
2018-05-31 20:15:49 -0400

    Adding some basic context to the plans for this file

--- www.spee.ch/docker-entrypoint.sh
+++ www.spee.ch/docker-entrypoint.sh
@@ -1,1 +1,4 @@
 #!/bin/ash
+## This compose file will take a copy of the configuration and install any
+## envvars and then copy any required files into the /app/ directory next to any
+## custom files added by the user.
