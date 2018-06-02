0aafe775ea6a8927eccbfca61332c79b36a34bdb
Leopere <colin@nixc.us>
2018-06-02 14:40:49 -0400

    Fail entrypoint better

--- www.spee.ch/docker-entrypoint.sh
+++ www.spee.ch/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/ash
+set -euo pipefail
 ## This compose file will take a copy of the configuration and install any
 ## envvars and then copy any required files into the /app/ directory next to any
 ## custom files added by the user.
