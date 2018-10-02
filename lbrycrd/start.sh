55ddbd409709d3ffb162faa8ec3f092da919ac9e
Leopere <colin@nixc.us>
2018-10-02 18:05:03 -0400

    A few issues fixed with permissions Fixed who runs the mkdir commands in Dockerfile Added make double check for directories existence in start.sh Still doesn't necessarily fix invocation. Corrected user shell to not include trailing slash

--- lbrycrd/start.sh
+++ lbrycrd/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 ## Ensure perms are correct prior to running main binary
+mkdir -p /data/lbrycrdd
+mkdir -p /etc/lbrycrdd
 chown -R 1000:1000 /data
 chmod -R 755 /data
 chown -R 1000:1000 /etc/lbrycrdd
