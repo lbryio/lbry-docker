04ba8f6c5ca07be37f67f144506bba9795ccafb6
Leopere <colin@nixc.us>
2018-10-03 13:39:28 -0400

    Committing start.sh This will likely be mostly an unnecessary file unless we get more flags For now it just asserts recursive dir perms.

---
+++ chainquery/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Ensure perms are correct prior to running main binary
+chown -R 1000:1000 /data
+chmod -R 755 /data
+
+## For now keeping this simple. Potentially eventually add all command args as envvars for the Dockerfile or use safe way to add args via docker-compose.yml
+~/chainquery
