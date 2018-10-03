c50e00ca568333cef270f96615a27c422500e3c2
Leopere <colin@nixc.us>
2018-10-03 13:44:30 -0400

    Added simple docker entrypoint for now.

---
+++ chainquery/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#!/bin/bash
+
+# default to run whatever the user wanted like "/bin/bash"
+## If user runs no need to run any more of the entrypoint script.
+if [[ -z "$@" ]]; then
+  echo "User did not attempt input. Now executing docker-entrypoint."
+else
+  echo "Running $@."
+  exec "$@"
+  exit 1
+fi
+
+/bin/bash
