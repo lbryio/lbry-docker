8b1ed11c3bb7ca4cdfd501efd9b08363b3bc2bd0
Leopere <colin@nixc.us>
2018-10-03 15:50:45 -0400

    Updated start

--- chainquery/start.sh
+++ chainquery/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## For now keeping this simple. Potentially eventually add all command args as envvars for the Dockerfile or use safe way to add args via docker-compose.yml
-chainquery
+chainquery serve
