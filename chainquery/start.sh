bfc73e687ac8c7c0ce1a7ceb6338b4e5693c21ef
Leopere <colin@nixc.us>
2018-10-03 14:35:44 -0400

    Not in the home dir here either

--- chainquery/start.sh
+++ chainquery/start.sh
@@ -5,4 +5,4 @@ chown -R 1000:1000 /data
 chmod -R 755 /data
 
 ## For now keeping this simple. Potentially eventually add all command args as envvars for the Dockerfile or use safe way to add args via docker-compose.yml
-~/chainquery
+chainquery
