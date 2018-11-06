83ef420db94d967414df9513779089430080bdf4
Leopere <colin@nixc.us>
2018-11-06 18:32:25 -0500

    Adding more comments

--- lbrycrd/start.sh
+++ lbrycrd/start.sh
@@ -10,12 +10,15 @@
 ## Ensure perms are correct prior to running main binary
 chown -R 1000:1000 /data
 chmod -R 755 /data
-chown -R 1000:1000 /etc/lbrycrd
-chmod -R 755 /etc/lbrycrd
+
+## TODO: Consider a config directory for future magic.
+# chown -R 1000:1000 /etc/lbrycrd
+# chmod -R 755 /etc/lbrycrd
 rm -f /var/run/lbrycrd.pid
 mkdir -p ~/.lbrycrd
 
 ## Set config params
+## TODO: Make this more automagic in the future.
 echo -e "rpcuser=lbryrpc\nrpcpassword=${RPC_PASSWORD:-changeme}" > $HOME/.lbrycrd/lbrycrd.conf
 echo -e "rpcallowip=${RPC_ALLOW_IP:-10.5.1.3}" >> $HOME/.lbrycrd/lbrycrd.conf
 echo -e "rpcuser=${RPC_USER:-lbryrpc}" >> $HOME/.lbrycrd/lbrycrd.conf
@@ -45,7 +48,7 @@ case ${RUN_MODE:-default} in
     ;;
 esac
 
-
+## TODO: Look into what we can do with these launch params
 ## We were unsure if these function as intended so they were disabled for the time being.
 #  -port=${PORT:-9246} \
 #  -data=${DATA_DIR:-/data/} \
