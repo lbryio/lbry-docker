c30c2d3840424068a5d8f67847ff5cb6f5395b69
root on chainquery dev <root@dev.chainquery.lbry.io>
2018-12-13 04:15:59 +0000

    work from Lenny and Beamer for Chainquery/Lbrycrd

--- lbrycrd/stuff/start.sh
+++ lbrycrd/stuff/start.sh
@@ -19,19 +19,22 @@ rm -f /var/run/lbrycrd.pid
 
 ## Set config params
 ## TODO: Make this more automagic in the future.
-echo "rpcuser=lbryrpc\nrpcpassword=$RPC_PASSWORD" > /data/.lbrycrd/lbrycrd.conf
+echo "rpcuser=$RPC_USER" > /data/.lbrycrd/lbrycrd.conf
+echo "rpcpassword=$RPC_PASSWORD" >> /data/.lbrycrd/lbrycrd.conf
 echo "rpcallowip=$RPC_ALLOW_IP" >> /data/.lbrycrd/lbrycrd.conf
-echo "rpcuser=$RPC_USER" >> /data/.lbrycrd/lbrycrd.conf
+echo "rpcport=9245" >> /data/.lbrycrd/lbrycrd.conf
+echo "rpcbind=0.0.0.0" >> /data/.lbrycrd/lbrycrd.conf
+#echo "bind=0.0.0.0" >> /data/.lbrycrd/lbrycrd.conf
 
 ## Control this invocation through envvar.
 case $RUN_MODE in
   default )
-    su -c "lbrycrdd -server -conf=/data/.lbrycrd/ -printtoconsole" lbrycrd
+    su -c "lbrycrdd -server -conf=/data/.lbrycrd/lbrycrd.conf -printtoconsole" lbrycrd
     ;;
   reindex )
-    su -c "lbrycrdd -server -txindex -reindex -conf=/data/.lbrycrd/ -printtoconsole" lbrycrd
+    su -c "lbrycrdd -server -txindex -reindex -conf=/data/.lbrycrd/lbrycrd.conf -printtoconsole" lbrycrd
     ;;
   chainquery )
-    su -c "lbrycrdd -server -txindex -conf=/data/.lbrycrd/ -printtoconsole" lbrycrd
+    su -c "lbrycrdd -server -txindex -conf=/data/.lbrycrd/lbrycrd.conf -printtoconsole" lbrycrd
     ;;
 esac
