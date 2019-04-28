77ee9dc39d7cec6598031805d0be9b7e80df834a
Leopere <colin@nixc.us>
2019-04-28 17:26:58 -0400

    Adding and enabling HEALTHCHECK

--- chainquery/stuff/healthcheck.sh
+++ chainquery/stuff/healthcheck.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-## TODO: Implement this at some point it requires CURL in the container.
-curl http://localhost:6300/api/status
+curl --fail http://localhost:6300/api/status || exit 1  
