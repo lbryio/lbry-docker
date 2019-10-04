f8ae8792e97b4769c54bdad84acbe6b313422f25
Dan Garthwaite <dan@garthwaite.org>
2019-10-04 18:38:44 -0400

    Working healthcheck in lbrycrd

--- lbrycrd/stuff/healthcheck.sh
+++ lbrycrd/stuff/healthcheck.sh
@@ -1,4 +1,1 @@
-## TODO: Implement a healthcheck for lbrycrd.
-curl --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getinfo","params":[]}' -H 'content-type:text/plain;'  http://$RPC_USER:$RPC_PASSWORD@127.0.0.1:9246
-## OR
-lbrycrd-cli getinfo
+/usr/bin/lbrycrd-cli -conf=/etc/lbry/lbrycrd.conf getnetworkinfo | grep -q '"networkactive": true,'
